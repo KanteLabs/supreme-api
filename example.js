edae9416ddf42c199650e0795eb323ee9e46498c
Amidou Kante <kamdiou@gmail.com>
2018-01-06 20:21:19 -0500

    created options.js to configure requests

--- example.js
+++ example.js
@@ -1,18 +1,5 @@
+var categories = require('./options')
 var api = require('./app');
-
-const categories = {
-    all: 'all',
-    new: 'new',
-    item: 'sweatshirts/riywuemd0/yy8sbq0kn',
-    category: {
-        sweatshirts: 'sweatshirts',
-        sweatshirts: 'sweatshirts',
-        sweatshirts: 'sweatshirts',
-        sweatshirts: 'sweatshirts',
-        sweatshirts: 'sweatshirts',
-    }
-}
-
 
 api.getAll(categories.all, (product, err) => {
     if (err) {
---
+++ options.js
@@ -0,0 +1,14 @@
+const categories = {
+    all: 'all',
+    new: 'new',
+    item: 'sweatshirts/riywuemd0/yy8sbq0kn',
+    category: {
+        sweatshirts: 'sweatshirts',
+        sweatshirts: 'sweatshirts',
+        sweatshirts: 'sweatshirts',
+        sweatshirts: 'sweatshirts',
+        sweatshirts: 'sweatshirts',
+    }
+}
+
+module.exports = categories;
